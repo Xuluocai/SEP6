c5e56734e85dc8734740ebbb5b8df934efd71681
Xuluocai <cxl1815093870@gmail.com>
2022-05-28 19:36:16 +0200

    Initial commit

---
+++ WebApplication1/Model/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class Movie
+    {
+        public Movie(){}
+        public Movie(String moviename,string directorname,string starname,string category) { }
+        public string moviename { get; set; }
+        public string directorname { get; set; }
+        public string starname { get; set; }
+        public string category { get; set; }
+        public double Rating { get; set; }
+        public string publishdate { get; set; }
+    }
+}
---
+++ WebApplication1/Model/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+
+        public User(String username, String password, String lastname, String firstname, String sex, int age) { }
+
+        public string username { get; set; }
+
+        public string password { get; set; }
+
+        public string birthday { get; set; }
+
+        public string lastname { set; get; }
+        public string firstname { get; set; }
+        public string sex { get; set; }
+        public int age { get; set; }
+
+    }
+}
+
